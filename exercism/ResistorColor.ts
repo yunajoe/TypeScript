b4d83b02647ceb30f966750ac0990e4cb5a02c65
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-12-08 07:59:22 +0900

    ResistorColor.ts

---
+++ exercism/ResistorColor.ts
@@ -0,0 +1,16 @@
+const colorCode = (color: string): number => {
+  return COLORS.indexOf(color);
+};
+
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
